89c0e0365e7ea9faf7f78096b4ff1c33e96bd568
HAZberi <mhazuberi@gmail.com>
2020-12-10 16:09:39 -0700

    ImageList Init

--- events-api-css/search-pics/src/components/App.js
+++ events-api-css/search-pics/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component{
     state = { images: [] }
@@ -30,6 +31,7 @@ class App extends React.Component{
             <div className = "ui container">
                 <SearchBar onSubmit={this.onSearchSubmit}/>
                 Found: {this.state.images.length} images
+                <ImageList images={this.state.images}/>
             </div>
         );
     }
