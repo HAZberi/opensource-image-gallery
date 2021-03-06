db830a4283f0da51b37513a0ba9bf3bd825e1d37
HAZberi <mhazuberi@gmail.com>
2021-03-05 20:41:41 -0700

    search-pics ready for CI/CD

--- events-api-css/search-pics/src/api/pexels.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.pexels.com/v1",
-  headers: {
-    Authorization: "563492ad6f917000010000019e71e1aa2ee241b6bc94a04ca1ca38a9",
-  },
-});
--- events-api-css/search-pics/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID g8dQej2zQ2DYDQpqQqhmOK4LOeM0V9HeRsvCiUdfR8g",
-  },
-});
